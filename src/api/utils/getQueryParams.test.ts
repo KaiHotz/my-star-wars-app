1d45edfc52214b1215018647dfb7521097f4c342
KaiHotz <kaihotz@me.com>
2024-06-27 23:53:26 +0400

    workflows added

--- src/api/utils/getQueryParams.test.ts
+++ src/api/utils/getQueryParams.test.ts
@@ -2,8 +2,6 @@ import { describe, expect, it } from 'vitest';
 
 import { getQueryParams } from './getQueryParams';
 
-const api_key = import.meta.env.VITE_API_KEY;
-
 const initialObj: Record<string, unknown> = {
   param_1: 'foo',
   param_2: 'bar',
@@ -11,12 +9,10 @@ const initialObj: Record<string, unknown> = {
   params_4: null,
   params_5: undefined,
   params_6: '',
-  api_key,
-  file_type: 'json',
 };
 
 describe('getQueryParams', () => {
   it('should return a query string', () => {
-    expect(getQueryParams(initialObj)).toEqual(`?param_1=foo&param_2=bar&api_key=${api_key}&file_type=json`);
+    expect(getQueryParams(initialObj)).toEqual(`?param_1=foo&param_2=bar`);
   });
 });
--- src/pages/Category/Category.tsx
+++ src/pages/Category/Category.tsx
@@ -24,6 +24,7 @@ export const Category: FC = () => {
   }, []);
 
   const handleDelete = useCallback((id: TCategory['id']) => {
+    // eslint-disable-next-line no-console
     console.log({ id });
   }, []);
 
