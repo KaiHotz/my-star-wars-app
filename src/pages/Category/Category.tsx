ce7ca9fd7e8dc73c687b81f2b2592f5f734df87b
KaiHotz <kaihotz@me.com>
2024-06-28 21:13:54 +0400

    category filter added

--- src/pages/Category/Category.tsx
+++ src/pages/Category/Category.tsx
@@ -1,10 +1,10 @@
-import { FC, useCallback, useEffect, useRef, useState } from 'react';
+import { ChangeEvent, FC, useCallback, useEffect, useRef, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useIntl } from 'react-intl';
 import cx from 'clsx';
+import { FaGrip, FaList, FaSistrix, FaXmark } from 'react-icons/fa6';
 import { TCategory, useCategoryList, useDeleteCategoryItem, useUpdateCategoryItem } from 'src/api';
-import { Button, Modal, Spinner } from 'src/ui-kit';
-import { FaGrip, FaList } from 'react-icons/fa6';
+import { Button, Input, Modal, Spinner } from 'src/ui-kit';
 import { Card } from 'src/components/Card';
 import { LoaderContainer } from 'src/components/LoaderContainer';
 import { messages } from 'src/dictionary';
@@ -16,6 +16,7 @@ export const Category: FC = () => {
   const cardRef = useRef<HTMLDivElement>(null);
   const { formatMessage: fm } = useIntl();
   const { category } = useParams();
+  const [search, setSearch] = useState<string>('');
   const [entryToEdit, setEntryToEdit] = useState<TCategory | null>(null);
   const [viewMode, setViewMode] = useState<'grid' | 'list'>('grid');
 
@@ -28,6 +29,8 @@ export const Category: FC = () => {
       cardRef.current?.scrollIntoView({ behavior: 'smooth' });
     }
   }, [data]);
+
+  const handleSearch = useCallback((e: ChangeEvent<HTMLInputElement>) => setSearch(e.target.value), []);
 
   const handleCloseModal = useCallback(() => {
     setEntryToEdit(null);
@@ -52,6 +55,10 @@ export const Category: FC = () => {
     [deleteItem],
   );
 
+  const categoryItems = data?.pages.map(({ results }) => results).flat();
+  const filteredItems = categoryItems?.filter(({ name, title }) =>
+    (name?.toLowerCase() || title?.toLowerCase())?.includes(search.toLowerCase()),
+  );
   const isGrid = viewMode === 'grid';
   const isPerson = category === 'people';
   const inProgress = isLoading || isFetching || isUpdating || isDeleting;
@@ -61,6 +68,18 @@ export const Category: FC = () => {
       <div className="category">
         {isDeleting && <LoaderContainer hasBackdrop />}
         <div className="category__header">
+          <div>
+            <Input
+              small
+              value={search}
+              onChange={handleSearch}
+              placeholder={fm(messages.searchPlaceholder)}
+              startAdornment={<FaSistrix />}
+              endAdornment={
+                search && <Button variant="ghost" onClick={() => setSearch('')} icon={<FaXmark size={16} />} />
+              }
+            />
+          </div>
           <Button
             variant="ghost"
             onClick={() => setViewMode(isGrid ? 'list' : 'grid')}
@@ -68,22 +87,20 @@ export const Category: FC = () => {
           />
         </div>
         <div className={cx(`category__body category__body--${viewMode}`)}>
-          {data?.pages.map(({ results }) =>
-            results.map((item, index) => {
-              const isLast = index === results.length - 1;
+          {filteredItems?.map((item, index) => {
+            const isLast = index === filteredItems.length - 1;
 
-              return (
-                <Card
-                  key={item.id}
-                  ref={isLast ? cardRef : undefined}
-                  data={item}
-                  variant={viewMode}
-                  onEdit={isPerson ? handleEntrytoEdit : undefined}
-                  onDelte={hanldeDeleteItem}
-                />
-              );
-            }),
-          )}
+            return (
+              <Card
+                key={item.id}
+                ref={isLast ? cardRef : undefined}
+                data={item}
+                variant={viewMode}
+                onEdit={isPerson ? handleEntrytoEdit : undefined}
+                onDelte={hanldeDeleteItem}
+              />
+            );
+          })}
         </div>
         <div className="category__footer">
           <Button variant="secondary" onClick={() => fetchNextPage()} disabled={!hasNextPage || inProgress}>
