13a6ea116b792233ca31952cb81a2fde8e6a55c6
KaiHotz <kaihotz@me.com>
2024-06-29 15:12:43 +0400

    show load more only if there is a next page

--- src/pages/Category/Category.tsx
+++ src/pages/Category/Category.tsx
@@ -107,9 +107,11 @@ export const Category: FC = () => {
           })}
         </div>
         <div className="category__footer">
-          <Button variant="secondary" onClick={() => fetchNextPage()} disabled={!hasNextPage || inProgress}>
-            {inProgress ? <Spinner size={16} /> : fm(messages.loadMore)}
-          </Button>
+          {hasNextPage && (
+            <Button variant="secondary" onClick={() => fetchNextPage()} disabled={inProgress}>
+              {inProgress ? <Spinner size={16} /> : fm(messages.loadMore)}
+            </Button>
+          )}
           {!hasReluts && !inProgress && <p>{fm(messages.noResults)}</p>}
         </div>
       </div>
